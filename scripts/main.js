63392e713e2437b785a21c2a36ac9bb98d31da4f
odpinerosh <odpinerosh@gmail.com>
2022-10-03 10:43:06 -0500

    Added Nav Bar Mail visibility

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,8 @@
 const deskTopMenu = document.getElementById('desktop-menu');
 const navBarMail = document.getElementById('nv-email');
 
+navBarMail.addEventListener('click', () => {
+    console.log('Se hizo un buen clic');
+    deskTopMenu.classList.toggle('visible');
+})
+
