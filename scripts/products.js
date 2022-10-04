85ee090ce180e19fc5563cf47259710f049615be
odpinerosh <odpinerosh@gmail.com>
2022-10-04 15:18:08 -0500

    Finished Product List

--- scripts/products.js
+++ scripts/products.js
@@ -32,16 +32,45 @@ const catListElectr = document.querySelector(".cat-list--electr");
 const catListFurnit = document.querySelector(".cat-list--furnit");
 const catListToys = document.querySelector(".cat-list--toys");
 const catListOther = document.querySelector(".cat-list--other");
+const cardsContainer = document.querySelector('.cards-container');
+const productsHead = document.querySelector('.products-head');
 
 const funProductFilter = (ev, cat, auxArr) => {
+    mobileMenuToggle();
     ev.preventDefault();
+    let resultArr = [];
     if (cat == 'All') {
-        result = auxArr.filter(product => product.cat != cat);
+        productsHead.classList.add('in-visible');
+        resultArr = auxArr.filter(product => product.cat != cat);
     } else {
-        result = auxArr.filter(product => product.cat == cat);
+        resultArr = auxArr.filter(product => product.cat == cat);
+        let aux = resultArr.length;
+        productsHead.classList.remove('in-visible');
+        productsHead.innerHTML = `Productos en esta categoría: ${aux}`;
     }
-    console.log(result);
+    cardsContainer.innerHTML = drawProductsGrid(resultArr);
 }
+
+const drawProductsGrid = arrProducts => {
+    let aux = "";
+    arrProducts.forEach(el => {
+        aux += `<div class="product-card">
+            <img src = ${el.image}>
+            <div class="product-info">
+                <div>
+                    <p>${el.price}</p>
+                    <p>${el.name}</p>
+                </div>
+                <figure>
+                    <img src="./icons/bt_add_to_cart.svg" alt="">
+                </figure>
+            </div>
+        </div>`;         
+    });
+    console.log(aux);
+    return aux;
+}
+
 
 catListAll.addEventListener('click', (event) => funProductFilter(event, 'All', productList));
 catListClothes.addEventListener('click', (event) => funProductFilter(event, 'Clothes', productList));
@@ -49,3 +78,4 @@ catListElectr.addEventListener('click', (event) => funProductFilter(event, 'Elec
 catListFurnit.addEventListener('click', (event) => funProductFilter(event, 'Furniture', productList));
 catListToys.addEventListener('click', (event) => funProductFilter(event, 'Toys', productList));
 catListOther.addEventListener('click', (event) => funProductFilter(event, 'Other', productList));
+
